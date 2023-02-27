b4b621f9a94e088f82fddd45068a092dbae42dfe
thedrgn <118586751+thedrgn@users.noreply.github.com>
2023-02-27 15:40:03 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(recordsArray){
+    const foundRecord = 
+    recordsArray.find(record => 
+        record.result === "W")
+    return foundRecord && foundRecord.year
+}
